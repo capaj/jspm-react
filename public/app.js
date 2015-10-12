7ad6e35de226b20d409285a9835a8edf4569859d
Jiří Špác <jiri.spac.external@socialbakers.com>
2015-10-12 19:48:10 +0200

    better flow in the index, proper language code logic

--- public/app.js
+++ public/app.js
@@ -11,8 +11,6 @@ window.ReactIntl = ReactIntl
 // https://github.com/zilverline/react-tap-event-plugin
 injectTapEventPlugin()
 
-// var knex = nexridge('http://localhost:8197')
-
 class RenderForcer extends React.Component {
   constructor () {
     super()
@@ -29,4 +27,8 @@ React.render((
   <RenderForcer/>
 ), document.getElementById('app'))
 
-System.import('jspm_packages/npm/react-intl@1.2.0/dist/locale-data/' + navigator.language)
+var lang = window.navigator.languages ? window.navigator.languages[0] : null
+lang = lang || window.navigator.language || window.navigator.browserLanguage || window.navigator.userLanguage
+if (lang.indexOf('-') !== -1) lang = lang.split('-')[0]
+if (lang.indexOf('_') !== -1) lang = lang.split('_')[0]
+System.import('jspm_packages/npm/react-intl@1.2.0/dist/locale-data/' + lang)
--- public/routes/home.js
+++ public/routes/home.js
@@ -8,7 +8,7 @@ export default class Home extends React.Component {
   render () {
     return <div>
       <h2>Home route</h2>
-      <p>you can try editing it if you like</p>
+      <p>you can try editing it as you like to test this out</p>
 
     </div>
   }
