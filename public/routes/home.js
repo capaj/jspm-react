b0f788e9529cd5128def17561fe5de6dd162f0dd
Jiří Špác <capajj@gmail.com>
2015-10-15 23:02:38 +0200

    fixed stateless style

--- public/routes/home.js
+++ public/routes/home.js
@@ -9,7 +9,7 @@ export default class Home extends React.Component {
   render () {
     return <div>
       <h2>Home route</h2>
-      <Stateless name='stranger'></Stateless>
+      <Stateless name='stranger'/>
       <p>you can try editing it as you like to test this out</p>
 
     </div>
