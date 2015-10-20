9b580b404b02b24b968a12e26e9d0750a5d16d3f
Jiří Špác <jiri.spac.external@socialbakers.com>
2015-10-20 21:23:47 +0200

    disable caching if not in production

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 'use strict'
 require('chokidar-socket-emitter')({port: 9081})
 const httpServer = require('http-server')
+
+let cache = 3600
+if (process.env.NODE_ENV === 'production') {
+  console.log('running in production mode(with caching)-make sure you have "Disable cache (while DevTools is open)" checked in the browser to see the changes while developing')
+} else {
+  cache = -1
+}
 const server = httpServer.createServer({
   root: 'public',
+  cache: cache,
   robots: true,
   headers: {
     'Access-Control-Allow-Origin': '*',
